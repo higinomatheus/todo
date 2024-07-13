40bead5a0a24bf756c3628d801452288414f08f9
Matheus Higino <matheus.higino@yahoo.com.br>
2024-07-13 10:05:31 -0300

    Criação da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo';
-}
+  public todos: any[] = [];
+  public title: String = 'Minas Tarefas';
+
+  constructor() {
+    this.todos.push('passear com o cachoro');
+    this.todos.push('ir ao supermercado');
+    this.todos.push('cortar o cabelo');
+    this.todos.push(1999);
+    this.todos.push(new Date());
+  }
+
+} 
