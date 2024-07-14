8c81941959110217b3354e07d0bee0274c1e36a1
Matheus Higino <matheus.higino@yahoo.com.br>
2024-07-14 17:01:32 -0300

    Removendo e alterando itens na lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,37 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Todo } from '../models/todo.model';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, NgFor],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, NgFor, NgIf],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css',
 })
 export class AppComponent {
-  public todos: Todo[] = [];
-  public title: String = 'Minas Tarefas';
+	public todos: Todo[] = [];
+	public title: String = 'Minas Tarefas';
 
-  constructor() {
-    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
-    this.todos.push(new Todo(2, 'Ir ao supermercado', true));
-    this.todos.push(new Todo(3, 'Cortar o cabelo', false));
-  }
+	constructor() {
+		this.todos.push(new Todo(1, 'Passear com o cachorro', false));
+		this.todos.push(new Todo(2, 'Ir ao supermercado', true));
+		this.todos.push(new Todo(3, 'Cortar o cabelo', false));
+	}
 
-} 
+	remove(todo: Todo) {
+		const index = this.todos.indexOf(todo);
+		if (index !== -1) {
+			this.todos.splice(index, 1);
+		}
+	}
+
+	markAsDone(todo: Todo) { 
+		todo.done = true;
+	}
+
+	markAsUndone(todo: Todo) {
+		todo.done = false;
+	 }
+}
