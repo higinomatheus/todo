9c025b31cda5f812e6b778290195e4ff6b0874ff
Matheus Higino <matheus.higino@yahoo.com.br>
2024-07-16 07:41:34 -0300

    Finalização da lógica do formulário e estilização do corpo da página

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,36 +2,71 @@ import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Todo } from '../models/todo.model';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet, NgFor, NgIf],
+	imports: [RouterOutlet, NgFor, NgIf, ReactiveFormsModule],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css',
 })
 export class AppComponent {
 	public todos: Todo[] = [];
-	public title: String = 'Minas Tarefas';
+	public title: String = 'Minhas Tarefas';
+	public form: FormGroup;
 
-	constructor() {
-		this.todos.push(new Todo(1, 'Passear com o cachorro', false));
-		this.todos.push(new Todo(2, 'Ir ao supermercado', true));
-		this.todos.push(new Todo(3, 'Cortar o cabelo', false));
+	constructor(private fb: FormBuilder) {
+		this.form = this.fb.group({
+			title: ['', Validators.compose([
+				Validators.minLength(3),
+				Validators.maxLength(60),
+				Validators.required
+			])]
+		});
+
+		setTimeout(() => this.load(), 500);
+	}
+
+	add() {
+		const title = this.form.controls['title'].value;
+		const id = this.todos.length + 1;
+		this.todos.push(new Todo(id, title, false));
+		this.save();
+		this.clear();
+	}
+
+	clear() {
+		this.form.reset();
 	}
 
 	remove(todo: Todo) {
 		const index = this.todos.indexOf(todo);
 		if (index !== -1) {
 			this.todos.splice(index, 1);
+			this.save();
 		}
 	}
 
-	markAsDone(todo: Todo) { 
+	markAsDone(todo: Todo) {
 		todo.done = true;
+		this.save();
 	}
 
 	markAsUndone(todo: Todo) {
 		todo.done = false;
-	 }
+		this.save();
+	}
+
+	save() {
+		const data = JSON.stringify(this.todos);
+		localStorage.setItem('todos', data);
+	}
+
+	load(){
+		console.log('localStorage', localStorage);
+		const data = localStorage.getItem('todos') || '';
+		const items = JSON.parse(data);
+		this.todos = items !== '' ? items : [];
+	}
 }
